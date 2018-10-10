7ccc3ed74956f0af36a73f37dcd92ec3774576d6
JChan <jcisbroke@gmail.com>
2018-10-09 20:39:28 -0700

    added 2 scrapers

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const fs = require('fs');
 const db = require('./models/connection.js');
+const scraperController = require('./scraper')
 
 // apply bodyParser and cookieParser at every route.
 app.use(bodyParser.json());
@@ -22,6 +23,16 @@ app.post('/event', (req, res) => {
       res.json(error);
     });
 });
+
+app.get('/builtinla', (req, res) => {
+  scraperController.getBuiltInLa()
+  .then(data => res.send(data))
+});
+
+app.get('/builtinny', (req, res) => {
+  scraperController.getBuiltInNy()
+  .then(data => res.send(data))
+})
 
 // initialize server at PORT 3000;
 app.listen(3000, () => {
