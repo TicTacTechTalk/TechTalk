9c79b0d4d7b8d96e4a1a43fcf2775fb350757dee
GodofCookery <leong.bryan.c@gmail.com>
2018-10-09 20:19:01 -0700

    database live and connected

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 // import dependencies
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const fs = require('fs');
+const db = require('./models/connection.js');
 
 // apply bodyParser and cookieParser at every route.
 app.use(bodyParser.json());
-app.use(cookieParser);
+app.use(cookieParser());
+
+app.post('/event', (req, res) => {
+  const { name, location, time } = req.body;
+  db.none('INSERT INTO events(name, location, time) VALUES($1, $2, $3)', [name, location, time])
+    .then(() => {
+      res.send('SUCCESS');
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
 
 // initialize server at PORT 3000;
 app.listen(3000, () => {
